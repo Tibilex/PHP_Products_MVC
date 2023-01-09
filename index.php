57600e37c954a800ee2465e7438897ed4f95ce51
Tibilex <45212175+Tibilex@users.noreply.github.com>
2023-01-09 20:09:01 +0200

    Initial Project

---
+++ index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="./CSS/style.css" rel="stylesheet">
+   <link href="https://fonts.googleapis.com/css?family=Roboto:300,regular,500,700&display=swap&subset=cyrillic-ext" rel="stylesheet" />
+   <title>Shop</title>
+</head>
+<body>
+<div class="container">
+   <header>
+      <div class="nav__bar">
+         <a class="nav__link" href="./Views/AdminPage.php">Админ панель</a>
+         <a class="nav__link" href="./Views/CartPage.php">Корзина</a>
+      </div>
+   </header>
+   <main>
+      <?php
+
+      ?>
+   </main>
+</div>
+</body>
+</html>
